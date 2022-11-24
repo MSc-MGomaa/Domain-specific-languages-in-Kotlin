8434405678bbeaaa2139857b28e55cb799ebfcbc
Mohamed Gomaa <mohammedsaber828@gmail.com>
2022-11-24 05:39:46 +0100

    Name Change from MCTRL to DSL

--- MCTSRL/src/Node.kt
+++
@@ -1,8 +0,0 @@
-class Node (pattern1 : List<List<Double>>){
-    val pattern = pattern1
-    var numberOfVisits : Double = 0.0
-    val rewardList = mutableListOf<Double>()
-    val children = mutableListOf<Node>()
-    var parent : Node? = null
-    val alreadyExpandedChildren = mutableListOf<Node>()
-}
